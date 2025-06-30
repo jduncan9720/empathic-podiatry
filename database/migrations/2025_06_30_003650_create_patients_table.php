ae0e43a288e42b1f62ee20ce59c89fce2b63cdad
Joshua Duncan <jduncan97@gmail.com>
2025-06-29 17:42:50 -0700

    Migrations

---
+++ database/migrations/2025_06_30_003650_create_patients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('date_of_birth')->nullable();
+            $table->string('room_number')->nullable();
+            $table->string('type_of_consent')->nullable();
+            $table->string('primary_insurance')->nullable();
+            $table->string('date_last_seen')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
